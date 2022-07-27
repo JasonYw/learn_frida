be548e9c3d2b6a03e79e676ec445481339edeeca
吴凯杰 <wkj14978@cnki.net>
2022-07-27 17:27:51 +0800

    20220727

--- catch.js
+++ catch.js
@@ -14,137 +14,162 @@ function bytesToHex(bytes) {
     return hex.join("");
 }
 
+//com.bytedance.retrofit2.client.Response.getBody
+function hook_response(){
+    var response = Java.use("com.bytedance.retrofit2.client.Response")
+    response.getBody.implementation = function(){
+        var result = this.getBody()
+        var arry = Java.use(result.$className)
+        var new_result = Java.cast(result,arry)
+        console.log('result =>',new_result,hexToString(bytesToHex(new_result.getBytes())))
+        return result
+    }
+}
+
+//com.bytedance.retrofit2.client.Request
+function hook_headers(){
+    var req = Java.use('com.bytedance.retrofit2.client.Request')
+    req.getHeaders.implementation= function(){
+        var result = this.getHeaders()
+        // var a = JSON.stringify(result)
+        // var b = JSON.parse(a)
+        var Map = Java.use('java.util.Collections$UnmodifiableRandomAccessList')
+        var NewP = Java.cast(result, Map)
+        console.log('header =>',NewP) 
+        return result
+    }
+}
+
+//com.bytedance.retrofit2.client.Request
+function hook_method(){
+    var req = Java.use('com.bytedance.retrofit2.client.Request')
+    req.getMethod.implementation = function(){
+        var result = this.getMethod()
+        console.log('method =>',result)
+        return result
+    }
+
+}
+
+//com.bytedance.retrofit2.client.Request
+function hook_url(){
+    var req = Java.use('com.bytedance.retrofit2.client.Request')
+    req.getUrl.implementation = function(){
+        var result = this.getUrl()
+        // var a = result.includes('https://aweme.snssdk.com/aweme/v2/video/safety/check/') 
+        // var b = result.includes('https://bsync.snssdk.com/v2%2Fpipeline?')
+        // var c = result.includes('https://aweme.snssdk.com/aweme/v1/aweme/stats/')
+        // var d = result.includes('https://aweme.snssdk.com/service/2/app_log/')
+        // var e = result.includes('aweme-poi/poi_loc/')
+        var f = result.includes('https://aweme.snssdk.com/aweme/v2/feed/')
+        if(f){
+            console.log('url =>',result)
+        }
+        return result
+    }
+}
+
+function hook_all(){
+    Java.enumerateLoadedClasses({
+        onMatch: function(name){
+            var clazz = Java.use(name);
+            console.log(name);
+            for(var method in clazz.class.getDeclaredMethods()){
+                console.log(method);
+            }
+            console.log(methods);    
+        },
+        onComplete: function(){}
+    })
+}
+
+function hook_all_sync(){
+    var classes = Java.enumerateLoadedClassesSync();
+    for (var i in classes){
+        var obj = Java.use(classes[i])
+        var methods = obj.class.getDeclaredMethods()
+        for(var j in methods){
+            console.log(classes[i],"=>",methods[j].getName());
+        }
+    }
+}
+
+
+function hook_ssl(){
+    var obj = Java.use('javax.net.ssl.TrustManagerFactory');
+    obj.getAlgorithm.implementation = function() {
+        var result  = this.getAlgorithm()
+        console.log('getAlgorithm =>',result)
+        return result
+    },
+    obj.getTrustManagers.implementation = function() {
+        var result  = this.getTrustManagers()
+        var arry = Java.use(result.$className)
+        var new_result = Java.cast(result,arry)
+
+        console.log('getTrustManagers =>',new_result)
+        return result
+    }
+    obj.getInstance.overload("java.lang.String").implementation = function(params){
+        var result  = this.getInstance(params)
+        console.log('getInstance =>',result)
+        return result
+
+    }
+
+}
+
+
+
+// javax.net.ssl.TrustManagerFactory.getInstance
+// function hook_ssl(){
+//     var obj = Java.use('javax.net.ssl.TrustManagerFactory');
+//     var methods = obj.class.getDeclaredMethods();
+//     for(var i in methods){
+//         var methodname =methods[i].getName(); 
+//         if(methodname =="init" && methodname =="instance"){
+//             console.log(methodname)
+//             // for(var j =0;j<obj.$init.overloads.length;j++){
+//             //     console.log(j)
+//             //     obj.$init.overloads[j].implementation = function(){
+//             //         var result = this.$init.apply(this,arguments)
+//             //         console.log(methods[i],'result =>',result)
+//             //         return result
+//             //     }
+//             // }
+
+//         }else{
+//             console.log(methodname)
+//             for(var k =0;k<obj[methodname].overloads.length;k++){
+//                 obj[methodname].overloads[k].implementation = function(){
+//                     var result = this[methodname].apply(this,arguments)
+//                     console.log(methods[i],'result =>',result)
+//                     return result
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
 
 function main() {
     Java.perform(function(){
-        // com.bytedance.retrofit2.client.Response.getBody
-        var response = Java.use("com.bytedance.retrofit2.client.Response")
-        response.getBody.implementation = function(){
-            var result = this.getBody()
-            var arry = Java.use(result.$className)
-            var new_result = Java.cast(result,arry)
-            console.log('result =>',new_result,hexToString(bytesToHex(new_result.getBytes())))
-            return result
-        }
-
-        // var req = Java.use('com.bytedance.retrofit2.client.Request')
-        // req.getHeaders.implementation= function(){
-        //     var result = this.getHeaders()
-        
-        //     // var a = JSON.stringify(result)
-        //     // var b = JSON.parse(a)
-        //     var Map = Java.use('java.util.Collections$UnmodifiableRandomAccessList')
-        //     var NewP = Java.cast(result, Map)
-        //     console.log('header =>',NewP) 
-        //     return result
-        // }
-        // req.getUrl.implementation = function(){
-        //     var result = this.getUrl()
-        //     // var a = result.includes('https://aweme.snssdk.com/aweme/v2/video/safety/check/') 
-        //     // var b = result.includes('https://bsync.snssdk.com/v2%2Fpipeline?')
-        //     // var c = result.includes('https://aweme.snssdk.com/aweme/v1/aweme/stats/')
-        //     // var d = result.includes('https://aweme.snssdk.com/service/2/app_log/')
-        //     // var e = result.includes('aweme-poi/poi_loc/')
-        //     var f = result.includes('https://aweme.snssdk.com/aweme/v2/feed/')
-        //     if(f){
-        //         console.log('url =>',result)
-        //     }
-        //     return result
-        // }
-
-        // req.getMethod.implementation = function(){
-        //     var result = this.getMethod()
-        //     console.log('method =>',result)
-        //     return result
-        // }
-        // req.getFirstHeader.overload('java.lang.String').implementation = function(str_){
-        //     var result = this.getFirstHeader(str_)
-        //     console.log('getFirstHeader args =>',str_)
-        //     console.log('getFirstHeader =>',result)
-        //     return result
-        // }
-        // var client = Java.use('com.bytedance.retrofit2.client.SsCall')
-        // client.execute.implementation = function(){
-        //     var result = this.execute()
-        //     console.log(result)
-        //     return result
-        // }
-        // client.cancel.implementation = function(){
-        //     var result = this.cancel()
-        //     console.log(result)
-        //     return result
-        // }
-        // client.getRequest.implementation = function(){
-        //     var result = this.getRequest()
-        //     console.log(result)
-        //     return result
-        // }
-        // client.setThrottleNetSpeed.implementation = function(long_){
-        //     var result = this.setThrottleNetSpeed(long_)
-        //     console.log(result)
-        //     return result
-        // }
-        // var client = Java.use('com.bytedance.retrofit2.SsHttpCall')
-        // client.getResponseWithInterceptorChain.implementation = function(){
-        //     var result = this.getResponseWithInterceptorChain()
-        //     var js_result = Java.cast(result,Java.use('com.bytedance.retrofit2.SsResponse'))
-        //     var header = Java.cast(js_result.headers(),Java.use(js_result.headers().$className))
-        //     var body = Java.cast(js_result.body(),Java.use(js_result.body().$className))
-        //     var raw_response 
-        //     console.log('header =>',header)
-        //     console.log('body =>',body)
-            
-
-            // shadow$_monitor_,hashCode,shadow$_klass_,identityHashCodeNative,wait,notify,notifyAll,equals,clone,identityHashCode,finalize,internalClone,toString,getClass,isSuccessful,_errorBody,message,code,success,rawResponse,retrofitMetrics,errorBody,headers,body,setRetrofitMetrics,_body,getRetrofitMetrics,error,raw,changeQuickRedirect
-            // return this.encrypt(s3)
-            // console.log(String.getBytes(JSON.stringify(result.rawResponse)))
-            // console.log('key =>',Object.keys(result))
-            // console.log('result1 =>',JSON.stringify(result.rawResponse))
-            
-            // var passwordAddr = new ptr(0xc00bda54)
-            // var passwordAddr = new ptr(0xf2d8fa8d)
- 
- 
-            // var pwd = Memory.readUtf8String(Memory.readPointer(passwordAddr));
-            // console.log(pwd);
-            // console.log(hexdump(new ptr(0xf2d8d475) ,{
-            //     offset: 0,
-            //     length: 64,
-            //     header: true,
-            //     ansi: true,
-            // }))
-            // return result
-
-        
-        // }
-        // client.enqueue.overload('com.bytedance.retrofit2.Callback').implementation = function(obj_){
-        //     var result = this.enqueue(obj_)
-        //     var a = JSON.stringify(obj_)
-        //     console.log('result =>',result,'args =>',a)
-        //     return result
-        // }
-
-
-        
+        hook_ssl();
     })
 }
 setImmediate(main)
-// com.bytedance.retrofit2.client.Request.safeCreateUri
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getBody()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getExtraInfo()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getFirstHeader(java.lang.String)
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getHeaders()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getHost()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getMaxLength()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getMethod()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getMetrics()
-// (agent) Hooking com.bytedance.retrofit2.client.Request.getPath()
 
 // (agent) Hooking com.bytedance.retrofit2.client.SsCall.cancel()
 // (agent) Hooking com.bytedance.retrofit2.client.SsCall.execute()   
 // (agent) Hooking com.bytedance.retrofit2.client.SsCall.getRequest()
 // (agent) Hooking com.bytedance.retrofit2.client.SsCall.setThrottleNetSpeed(long)
-
-// 3367397876
-// 4074296437
-// 4074306189
+// public final java.lang.String javax.net.ssl.TrustManagerFactory.getAlgorithm()
+// public final java.security.Provider javax.net.ssl.TrustManagerFactory.getProvider()
+// public final javax.net.ssl.TrustManager[] javax.net.ssl.TrustManagerFactory.getTrustManagers()
+// public final void javax.net.ssl.TrustManagerFactory.init(java.security.KeyStore) throws java.security.KeyStoreException
+// public final void javax.net.ssl.TrustManagerFactory.init(javax.net.ssl.ManagerFactoryParameters) throws java.security.InvalidAlgorithmParameterException
+// public static final java.lang.String javax.net.ssl.TrustManagerFactory.getDefaultAlgorithm()
+// public static final javax.net.ssl.TrustManagerFactory javax.net.ssl.TrustManagerFactory.getInstance(java.lang.String) throws java.security.NoSuchAlgorithmException
+// public static final javax.net.ssl.TrustManagerFactory javax.net.ssl.TrustManagerFactory.getInstance(java.lang.String,java.lang.String) throws java.security.NoSuchAlgorithmException,java.security.NoSuchProviderException
+// public static final javax.net.ssl.TrustManagerFactory javax.net.ssl.TrustManagerFactory.getInstance(java.lang.String,java.security.Provider) throws java.security.NoSuchAlgorithmException
