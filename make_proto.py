45cf041ca8ee6ba128b13f9e100a36a604d0ac69
吴凯杰 <wkj14978@cnki.net>
2022-08-02 11:31:38 +0800

    20220802

--- make_proto.py
+++ make_proto.py
@@ -3,21 +3,24 @@ import os
 
 
 proto_java_path = 'a.txt'
-output_path = 'x.proto'
-
 proto_java_lines = open(proto_java_path,'r').readlines()
+class_ = proto_java_lines[0].replace('\n','')
+output_path = f'{class_}.proto'
 output = open(output_path,'w')
 output.write('syntax = "proto3";\n')
-for line in proto_java_lines:
+output.write(f'message {class_} '+'{\n')
+for line in proto_java_lines[1:]:
     #ProtoAdapter.INT32.encodeWithTag(protoWriter, 1, status_code(feedItemList));
     try:
-        a = re.findall(r'\.(.+)\.encodeWithTag\(protoWriter, (\d+), (.+)\(',line)[0]
-        type_ = a[0].lower()
-        num = int(a[1])
-        name = a[2]
-        print(f'{type_} {name} = {num};')
-        output.write(f'{type_} {name} = {num};\n')
+        if line.startswith('ProtoAdapter'):
+            a = re.findall(r'\.(.+)\.encodeWithTag\(protoWriter, (\d+), (.+)\(',line)[0]
+            type_ = a[0].lower()
+            num = int(a[1])
+            name = a[2]
+            output.write(f'\t{type_} {name} = {num};\n')
+        else:
+            output.write(f'\t//{line}')
     except:
-        print(f'{line}')
-        output.write(f'{line}\n')
+        output.write(f'\t//{line}')
+output.write('}')
 output.close()
