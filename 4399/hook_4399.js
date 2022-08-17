4fe149e7887c4fa04b9fb4fa4fac51a6961bed9a
rico wu <jasonwu@outlook.com>
2022-08-17 22:37:03 +0800

    20220817

--- 4399/hook_4399.js
+++ 4399/hook_4399.js
@@ -1,39 +1,5 @@
 //com.m4399.gamecenter
 
-
-function hook_update(){
-    Java.perform(function(){
-        var upgrade_ = Java.use('com.upgrade.AppUpgradeManager')
-        upgrade_.checkUpgrade.implementation =function(a){
-            console.log(1)
-            // var  result = this.AppUpgradeDialog(a,b)
-            // return result
-        }
-    })
-
-}
-function hook_kill(){
-    Java.perform(function(){
-        var kill_ = Java.use('com.framework.utils.AppUtils')
-        kill_.killCurrentProcess.implementation = function(){
-            console.log('hook kill')
-        }
-    })
-}
-
-
-function hook_chekupgradecondition(){
-    Java.perform(function(){
-        var check_condition = Java.use('com.upgrade.AppUpgradeManager')
-        check_condition.checkUpgradeCondition.implementation = function(){
-            console.log(2)
-            return false
-        }
-    })
-}
-
-
-
 function showStacks() {
     Java.perform(function(){
         console.log(
@@ -57,61 +23,83 @@ function hook_toast(){
     })
 }
 
-function hook_netwrok(){
-    Java.perform(function(){
-        var net_ = Java.use('com.framework.manager.network.NetworkStats')
-        net_.networkAvalible.implementation = function(){
-            return true
-        }
-    })
-}
 
-function hook_file(){
-    Java.perform(function(){
-        var file_ = Java.use('java.io.File')
-        for(var i =0;i<file_.$init.overloads.length;i++){
-            file_.$init.overloads[i].implementation = function(){
-                var result = this.$init.apply(this,arguments)
-                console.log('args =>',JSON.stringify(arguments))
-                showStacks()
-                return result
-            }
-        }
-    })
-}
-
-
-function hook_downmodel(){
-    Java.perform(function(){
-        var downmodel = Java.use('com.download.DownloadModel')
-        // var long = Java.use('java.lang.Long')
-        downmodel.getExtra.overload('java.lang.String','java.lang.Object').implementation = function(a,b){
-            console.log(a,JSON.stringify(b))
-            var result = this.getExtra(a,b)
-            showStacks()
-            return result
-            
-        }
-    })
-}
 
 function hook_login(){
     Java.perform(function(){
-        var buildparam = Java.use('com.m4399.gamecenter.plugin.main.providers.be.d')
-        buildparam.buildRequestParams.implementation = function(a,b){
-            var b_ = Java.cast(b,Java.use('java.util.HashMap'))
-            console.log(a,b_,JSON.stringify(b))
-            var  result = this.buildRequestParams(a,b)
-            return result
+        var buildparam = Java.use('com.m4399.gamecenter.plugin.main.providers.be.al')
+        buildparam.setPassword.implementation = function(a){
+            console.log(a)
+            return this.setPassword(a)
+        }
+        Java.choose('com.m4399.gamecenter.plugin.main.providers.be.al',{
+            onMatch:function(instance){
+                console.log(instance.dsf.value)
+            },
+            onComplete:function(){
+
+            }
+        })
+            
+        
+    })
+}
+
+
+
+
+function hook_so_func(addr){
+    var so_addr = Module.findBaseAddress("libNativeHelper.so")
+    var func_addr = so_addr.add(addr)
+    Interceptor.attach(func_addr,{
+        onEnter:function(args){
+            console.log(addr,'enter =>')
+            this.args = args
+            console.log(hexdump(args[0]))
+            console.log(hexdump(args[1]))
+            console.log(hexdump(args[2]))
+
+        },
+        onLeave:function(retval){
+            console.log(addr,'leave =>')
+            console.log(hexdump(this.args[0]))
+            console.log(hexdump(this.args[1]))
+            console.log(hexdump(this.args[2]))
+            console.log('result =>',retval)
+
         }
     })
 }
 
-hook_login()
-// hook_update()
-// hook_file()
-// hook_toast()
-// hook_kill()
+
+hook_so_func(0x71fc)
+hook_so_func(0x78b0)
+hook_so_func(0x22bc)
+hook_so_func(0x46c0)
+hook_so_func(0x2454)
+hook_so_func(0x1fec)
+hook_so_func(0x5ae4)
+hook_so_func(0x4a4c)
+hook_so_func(0x4c54)
+hook_so_func(0x596c)
 
 
+    //   /* TID 0x1acb */
+    //   14407 ms  sub_71fc()
+    //   14407 ms     | sub_78b0()
+    //   14407 ms     | sub_22bc()
+    //   14407 ms     | sub_46c0()
+    //   14407 ms     |    | sub_2454()
+    //   14408 ms     |    | sub_2454()
+    //   14408 ms     | sub_1fec()
+    //   14408 ms  sub_5ae4()
+    //   14408 ms     | sub_4a4c()
+    //   14409 ms     |    | sub_4c54()
+    //   14409 ms     | sub_596c()
+    //   14409 ms     |    | sub_4a4c()
+    //   14409 ms     |    | sub_4a4c()
+    //   14409 ms     |    |    | sub_4c54()
+
+
+// hook_login()
 // android hooking watch class_method com.android.okhttp.CertificatePinner$Builder.add --dump-args --dump-backtrace --dump-return
