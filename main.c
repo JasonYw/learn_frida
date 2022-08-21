f9a10a7ffb676404b56a7dd1916d664186ed0fa7
rico wu <jasonwu@outlook.com>
2022-08-21 22:27:40 +0800

    20220821

--- main.c
+++ main.c
@@ -1,103 +1,21 @@
-#include <string.h>
 #include <stdio.h>
-#include "MD5.h"
-#include "SHA1.h"
-#include "SHA256.h"
-#include "SHA512.h"
-#include "HMAC_MD5.h"
 
-void call_md5() {
-    MD5_CTX context;
-    MD5Init(&context);
-    unsigned char *plainText = (unsigned char *) "xiaojianbang";
-    MD5Update(&context, plainText, strlen(plainText));
-    unsigned char result[16];
-    MD5Final(&context, result);
 
-    char temp[2] = {0};
-    char finalResult[32] = {0};
-    for (int i = 0; i < 16; i++) {
-        int index = i;
-        sprintf(temp, "%.2x", result[index]);
-        strcat(finalResult, temp);
-    }
-    printf("md5：%s\n", finalResult);
-}
-
-void call_sha1() {
-    SHA1Context context;
-    SHA1Reset(&context);
-    unsigned char *plainText = (unsigned char *) "xiaojianbang";
-    SHA1Input(&context, plainText, strlen(plainText));
-    unsigned char result[20];
-    SHA1Result(&context, result);
-
-    char temp[2] = {0};
-    char finalResult[40] = {0};
-    for (int i = 0; i < 20; i++) {
-        int index = i;
-        sprintf(temp, "%.2x", result[index]);
-        strcat(finalResult, temp);
-    }
-    printf("sha1：%s\n", finalResult);
-}
-
-void call_sha256() {
-    SHA256_CTX context;
-    sha256_init(&context);
-    unsigned char *plainText = (unsigned char *) "xiaojianbang";
-    sha256_update(&context, plainText, strlen(plainText));
-    unsigned char result[32];
-    sha256_final(&context, result);
-
-    char temp[2] = {0};
-    char finalResult[64] = {0};
-    for (int i = 0; i < 32; i++) {
-        int index = i;
-        sprintf(temp, "%.2x", result[index]);
-        strcat(finalResult, temp);
-    }
-    printf("sha256：%s\n", finalResult);
-}
-
-void call_sha512() {
-    SHA512_CB context;
-    SHA512Init(&context);
-    unsigned char *plainText = (unsigned char *) "xiaojianbang";
-    SHA512Update(&context, plainText, strlen(plainText));
-    unsigned char result[64];
-    SHA512Final(&context, result);
-
-    char temp[2] = {0};
-    char finalResult[128] = {0};
-    for (int i = 0; i < 64; i++) {
-        int index = i;
-        sprintf(temp, "%.2x", result[index]);
-        strcat(finalResult, temp);
-    }
-    printf("sha512：%s\n", finalResult);
-}
-
-void call_hmac_md5() {
-    unsigned char* plainText = (unsigned char *) "xiaojianbang";
-    unsigned char* key = (unsigned char *) "a12345678";
-    unsigned char result[16];
-    hmac_md5(result, plainText, strlen(plainText), (unsigned char *) key, strlen(key));
-    char temp[2] = {0};
-    char finalResult[32] = {0};
-    for (int i = 0; i < 16; i++) {
-        int index = i;
-        sprintf(temp, "%.2x", result[index]);
-        strcat(finalResult, temp);
-    }
-    printf("hamc_md5：%s\n", finalResult);
-}
-
-int main() {
-    call_md5();
-    call_sha1();
-    call_sha256();
-    call_sha512();
-    call_hmac_md5();
-    return 0;
+int main ()
+{
+   int  var = 20;   /* 实际变量的声明 */
+   int  *ip;        /* 指针变量的声明 */
+ 
+   ip = &var;  /* 在指针变量中存储 var 的地址 */
+ 
+   printf("var 变量的地址: %p\n", &var  );
+ 
+   /* 在指针变量中存储的地址 */
+   printf("ip 变量的值: %p\n", ip );
+ 
+   /* 使用指针访问值 */
+   printf("*ip 变量的值: %d\n", *ip );
+   printf("ip 变量的地址: %p\n", &ip);
+ 
+   return 0;
 }
