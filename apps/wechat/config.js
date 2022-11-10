4f55e91ae323d4fc3a1689cdd2200341a14b829d
rico <jaosnwu@outlook.com>
2022-11-10 09:38:34 +0800

    20221110

--- apps/wechat/config.js
+++ apps/wechat/config.js
@@ -1,3 +1,9 @@
+var host ="{host}"
+var port ="{port}"
+var MobileID = "{MobileID}"
+var PackageName = "{PackageName}"
+var req = "http://" + host + ":" + port + "/app/hook/data?MobileID=" + MobileID + "&PackageName=" + PackageName 
+
 Java.perform(function(){
     // 完整代码：
 
@@ -20,8 +26,15 @@ Java.perform(function(){
         var chksm = a.indexOf("&chksm=") != -1
         if(biz && mid && idx && sn && chksm){
             console.log(a)
+            send_wechat(a)
         }
         return this.parsePath(a,b)
+    }
+
+    Log.i.overload('java.lang.String', 'java.lang.String').implementation = function(a,b){
+        send_wechat("start hook wechat by beta1")
+        a = a + ':hooked:' + req
+        return this.i(a,b)
     }
 
     SQLiteDatabase.insertWithOnConflict.implementation = function (arg1, arg2, arg3, i) {
@@ -42,10 +55,25 @@ Java.perform(function(){
                 var res = JSONObject.toJSONString(str_);
                 var data = JSONObject.parseObject(res)
                 var value_data = data.getString(".msg.appmsg.mmreader.category.item.url")
-                console.log(value_data)
+                send_wechat(value_data)
             }
         }
         return ret;
     }
 })
 
+
+function send_wechat(send_msg){
+    Java.perform(function(){
+        var OkHttpClient =  Java.use("com.android.okhttp.OkHttpClient");
+        var Builder = Java.use("com.android.okhttp.Request$Builder").$new();
+        var RequestBody = Java.use("com.android.okhttp.RequestBody").$new();
+        var post_body = Java.use("java.lang.String").$new(send_msg)
+        console.log(post_body)
+        var media_type = Java.use("com.android.okhttp.MediaType").$new("","","","").parse(Java.use("java.lang.String").$new("application/x-www-form-urlencoded"))
+        var body = RequestBody.create(media_type, post_body)
+        var request = Builder.url(Java.use("java.lang.String").$new(req)).post(body).build();
+        OkHttpClient.$new().newCall(request).execute();
+
+    })
+}
