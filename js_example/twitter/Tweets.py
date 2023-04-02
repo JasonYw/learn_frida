7e11e7c39246c1da514e30bb94ea5af95d189883
rico_wu <jasonwu@outlook.com>
2023-04-02 13:58:02 +0800

    20230402

--- js_example/twitter/Tweets.py
+++ js_example/twitter/Tweets.py
@@ -11,7 +11,7 @@ class Tweets:
         self.features = {"responsive_web_twitter_blue_verified_badge_is_enabled": True, "responsive_web_graphql_exclude_directive_enabled": True, "verified_phone_label_enabled": False, "responsive_web_graphql_timeline_navigation_enabled": True, "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False, "tweetypie_unmention_optimization_enabled": True, "vibe_api_enabled": True, "responsive_web_edit_tweet_api_enabled": True, "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
                     "view_counts_everywhere_api_enabled": True, "longform_notetweets_consumption_enabled": True, "tweet_awards_web_tipping_enabled": False, "freedom_of_speech_not_reach_fetch_enabled": False, "standardized_nudges_misinfo": True, "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False, "interactive_text_enabled": True, "responsive_web_text_conversations_enabled": False, "longform_notetweets_richtext_consumption_enabled": False, "responsive_web_enhance_cards_enabled": False}
         self.session = requests.session()
-        self.x_guest_token,self.cookie = getGuestToken(self.session)
+        self.refreshToken()
         self.base_headers = {
             'authority': 'twitter.com',
             'path': self.url[19:],
@@ -22,6 +22,10 @@ class Tweets:
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
         }
         self.next_cursor = None
+        self.data = None
+    
+    def refreshToken(self):
+        self.x_guest_token,self.cookie = getGuestToken(self.session)
     
     def crawl(self) -> None:
         self.base_headers.update({
@@ -39,7 +43,7 @@ class Tweets:
             self.next_cursor = None
         else:
             self.next_cursor = data[-1]["content"]["value"]
-
+        self.data = data[:-2]
 
 if __name__ == "__main__":
     Tweets().crawl()
