a72fbd7782fdf1a615853ea8f53110ab6d6a7669
rico_wu <jasonwu@outlook.com>
2024-11-21 06:43:24 +0800

    20241121

--- encode/HMAC_MD5.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef ENCRYPTDEMO_HMAC_MD5_H
-#define ENCRYPTDEMO_HMAC_MD5_H
-
-void hmac_md5(unsigned char *out, unsigned char *data, int dlen, unsigned char *key, int klen);
-
-#endif //ENCRYPTDEMO_HMAC_MD5_H
